ef776c562e63767fb15b1a8003db8aa03426887b
Mahammad Haneef Gorantla <55850551+haneefnani@users.noreply.github.com>
2020-08-08 14:17:17 +0530

    Update start.py

--- www.templepurohit.com/start.py
+++ www.templepurohit.com/start.py
@@ -1,57 +1,57 @@
 from requests import get
 import bs4
-import csv    
+import csv
+csv_file=open('abc.csv','a')
+csv_writer=csv.writer(csv_file,quoting=csv.QUOTE_ALL)
 
 with open('urls.txt') as file:
     urls = file.readlines()
+# print(len(urls))
 
-csv_file = open('abc.csv', 'a')
-csv_writer = csv.writer(csv_file)
-
-count=0
-for url in urls[223:226]:
-    print(f'{count} running')
+for url in urls:
     res = get(url[:-1])
     response = bs4.BeautifulSoup(res.text, 'html.parser')
-    heading = response.find('h1', class_='entry-title').text
-    matter = response.find('div', class_='entry-summary')
-    for i in matter.findAll('p'):
-        # print(i.text)
-        luuu = i.text.replace('\t', ' ').split('\n')
-        lww = []
-        testing_for_missing = []
-        fixed = ['Locality/village', 'State', 'Country', 'Nearest City/Town',
-                'Best Season To Visit', 'Languages', 'Temple Timings', 'Photography']
-
-        for i in luuu:
-            # print(i)
-            lww.append(i.split(':')[1].strip())
-            testing_for_missing.append(i.split(':')[0].strip())
-        got = set(testing_for_missing)
-        result = set(fixed) - got
-        if len(list(result)) > 0:
-            lww.insert(fixed.index(list(result)[0]), '')
-        # print(lww)
+    # print(response.prettify())
     
-    images = []
-    images.append(response.find('div', class_='page-inner').img['src'])
-    try:
-        image=response.find('div', class_='panel active').figure.a.img['src']
-        images.append(image)
-    except:
-        pass
+    lppp=[]
+    heading=response.find('h1', class_='entry-title').text
+    matter=response.find('div',class_='entry-summary')
+    # with open('bla.txt','w') as fi:
+    for i in matter.findAll('p'):
+        # print(i.text,'\n')
+        lppp.append(i.text)
+    joined_word=' '.join(lppp)
+    word=joined_word.replace('\t',' ').split('\n')
+    word_copy=word
+    # print(word_copy)
+    for_removal=[]
+    for i in word:
+        if (i.endswith('Morning')):
+            place=word.index('Temple Timings : Morning')
+            tttt =' '.join(word[word.index('Temple Timings : Morning') : word.index('Temple Timings : Morning')+3])
+            for u in range(word.index('Temple Timings : Morning'),word.index('Temple Timings : Morning')+3):
+                # print(word[u])
+                for_removal.append(word[u])
+            for i in for_removal:
+                word_copy.remove(i)
+            word_copy.insert(place,tttt)
 
-    loo = []
-    for i in response.find('div', class_='panel active'):
-        print(i.text)
+
+    lww=[]
+    for i in word_copy:
+        lww.append(i.split(':')[1].strip())
+    print(lww)
+    loo=[]
+    for i in response.find('div',class_='panel active'):
+    #   print(i.text)
         loo.append(i.text)
-        # file.write(i.text)
-        # for j in i.findAll('p'):
-        # print(j.text)
-    # print(len(loo))
-
-    count+=1
-    csv_writer.writerow([heading]+lww+[' '.join(loo)]+images)
-    # print((lww))
-
+    # file.write(i.text)
+    for j in i.findAll('p'):
+        loo.append(j.text)
+# print(len(loo))
+# 
+# 
+    csv_writer.writerow([heading]+lww+[' '.join(loo)])
+# print((lww))
+# print([heading]+lww+[' '.join(loo)])
 csv_file.close()
