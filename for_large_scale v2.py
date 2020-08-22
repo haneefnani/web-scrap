423b8729f8c56d6a1823462a79540ef59798964c
Mahammad Haneef Gorantla <55850551+haneefnani@users.noreply.github.com>
2020-08-22 12:55:01 +0530

    Update for_large_scale v2.py

--- for_large_scale v2.py
+++ for_large_scale v2.py
@@ -52,57 +52,60 @@ file2.close()
 for i, url in enumerate(loo):
     this_url=url[:-1]
     if this_url not in urls:
-        print(f'\n{i}th URL\n')
-        driver.get(this_url)
-        response = bs4.BeautifulSoup(driver.page_source, 'html.parser')
-        file_name = response.find('h1', class_='noMarg').text+'.csv'
-        file = open('./csv/glassdoor/'+file_name, 'w', encoding='utf-8')
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(['Question', 'Answers-->'])
-        number_in_page = int(response.find('div', class_='reviewCount cell span-1-2 drop').text.replace(',', ''))
-        if (number_in_page // 10 ) in [0,1]:
-            no_of_pages=number_in_page // 10 + 1
-        else:
-            no_of_pages = number_in_page // 10 + 1 + 4
-        current_page = 1
-        for num in range(no_of_pages):
-
+        try:
+            print(f'\n{i}th URL\n')
+            driver.get(this_url)
             response = bs4.BeautifulSoup(driver.page_source, 'html.parser')
-            print(f'{current_page}th page')
-            a = 0
-            for i in response.findAll('div', class_='interviewQuestionWrapper padVertLg'):
-
-                l, l2 = [], []
-                try:
-                    # print('\n Heading : ',i.find('p',class_='questionText h3').text+'\n')
-                    l.append(i.find('p', class_='questionText h3').text.strip())
-                except:
-                    pass
-                try:
-                    # for remaining answers
-                    for j in i.findAll('p', class_='cell noMargVert padVert borderBot'):
-                        # print(a+1,j.text)
-                        l2.append(j.text.strip())
-                except:
-                    pass
-                try:
-                    # for first answer
-                    for j in i.findAll('p', class_='cell noMargVert padTop tightBot'):
-                        # print(a+1,j.text)
-                        l2.append(j.text.strip())
-                        # a+=1
-                except:
-                    pass
-                csv_writer.writerow(l+l2)
-            current_page += 1
-
-            print('going to next page......')
-            # driver.get('http://glassdoor.co.in'+response.find('li',class_='next').a['href'])
-            if current_page != no_of_pages+1 :
-                try:
-                    driver.get(this_url[:-4] + '_IP' + str(num) + this_url[-4:])
-                except:
-                    print('\n\nStopped------------------------------------------------------------------->')
+            file_name = response.find('h1', class_='noMarg').text+'.csv'
+            file = open('./csv/glassdoor/'+file_name, 'w', encoding='utf-8')
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(['Question', 'Answers-->'])
+            number_in_page = int(response.find('div', class_='reviewCount cell span-1-2 drop').text.replace(',', ''))
+            if (number_in_page // 10 ) in [0,1]:
+                no_of_pages=number_in_page // 10 + 1
             else:
-                print('-------------------last page--------------------------')
-        file.close()
+                no_of_pages = number_in_page // 10 + 1 + 4
+            current_page = 1
+            for num in range(no_of_pages):
+
+                response = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+                print(f'{current_page}th page')
+                a = 0
+                for i in response.findAll('div', class_='interviewQuestionWrapper padVertLg'):
+
+                    l, l2 = [], []
+                    try:
+                        # print('\n Heading : ',i.find('p',class_='questionText h3').text+'\n')
+                        l.append(i.find('p', class_='questionText h3').text.strip())
+                    except:
+                        pass
+                    try:
+                        # for remaining answers
+                        for j in i.findAll('p', class_='cell noMargVert padVert borderBot'):
+                            # print(a+1,j.text)
+                            l2.append(j.text.strip())
+                    except:
+                        pass
+                    try:
+                        # for first answer
+                        for j in i.findAll('p', class_='cell noMargVert padTop tightBot'):
+                            # print(a+1,j.text)
+                            l2.append(j.text.strip())
+                            # a+=1
+                    except:
+                        pass
+                    csv_writer.writerow(l+l2)
+                current_page += 1
+
+                print('going to next page......')
+                # driver.get('http://glassdoor.co.in'+response.find('li',class_='next').a['href'])
+                if current_page != no_of_pages+1 :
+                    try:
+                        driver.get(this_url[:-4] + '_IP' + str(num) + this_url[-4:])
+                    except:
+                        print('\n\nStopped------------------------------------------------------------------->')
+                else:
+                    print('-------------------last page--------------------------')
+            file.close()
+        except:
+            pass
